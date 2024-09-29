af9d3f46e513b6fa54afb6f38408fa8b1fd54218
tito moctezuma <tatoruami@protonmail.com>
2024-09-29 15:04:10 -0500

    uptd new code

--- code.js
+++ code.js
@@ -48,3 +48,44 @@ const randoShiftValue =
   Math.floor(randoNum * (highSiftValue - lowShifValue + 1)) + lowShifValue;
 
 console.log(randoShiftValue);
+
+const emblemClub1 = "Eagle";
+const emblemClub2 = "Laurel";
+const emblemClub3 = 7;
+let partyLoc = "";
+
+if (emblemClub1 === "Eagle") {
+  partyLoc = "Forum";
+} else if (emblemClub1 === "Lion") {
+  partyLoc = "Colosseum";
+} else {
+  partyLoc = "Villa";
+}
+
+if (emblemClub2 === "Laurel" && partyLoc === "Forum") {
+  partyLoc += " Augustus";
+} else if (emblemClub2 === "Grapes" || partyLoc === "Villa") {
+  partyLoc += " Of Pompey";
+}
+
+switch (emblemClub3) {
+  case 7:
+    location += " North";
+    break;
+  case 3:
+    location += " South";
+    break;
+  case 9:
+    location += " East";
+    break;
+  case 4:
+    location += " West";
+    break;
+}
+
+/*Question: is esentiall to know  how to use == and === since they are
+a form to understand how equality is determined Ex:
+1 == "1" is true because "1" is converted to a number
+where 
+1 === "1" is false because they are of different type
+*/
